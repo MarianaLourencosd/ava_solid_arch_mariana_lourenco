734142bba7174511bcacb20cb6d8bdb88177e42a
Mariana Lourenço <marianalourencosd@gmail.com>
2026-05-11 10:30:21 -0300

    Task 1: estruturação das rotas de Pet - Cria PetRoutes.js com todas as rotas - Adiciona middlewares verifyToken e imageUpload - Configura index.js com rota /pets

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const UserRouters = require('./routers/UserRouters')
-
+const PetRoutes = require('./routers/PetRoutes')
 const app = express()
 
 app.use(express.json())
@@ -10,6 +10,7 @@ app.use(cors({ credentials: true, origin: 'http://localhost:3000' }))
 
 app.use(express.static('public'))
 
+app.use('/pets', PetRoutes)
 app.use('/users', UserRouters)
 
 app.listen(5000)
